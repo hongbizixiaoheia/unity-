e0b379a2853ecc9552d53cbc0f65e41404240783
hongbizixiaoheia <2502700840@qq>
2022-05-11 19:51:22 +0800

    1.0

---
+++ Assets/1.0/Sprites/Gun.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject rocket; //
+    public float speed = 20f;
+    PlayeCtrl playerCtrl;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCtrl = transform.root.GetComponent<PlayeCtrl>();//获取playerCtrl脚本
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1")) //判断开火
+        {
+            if (playerCtrl.bFaceRight)
+            {
+               
+                GameObject bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(0, 0, 0));
+                Rigidbody2D bi = bulletInstance.GetComponent<Rigidbody2D>();
+                bi.velocity = new Vector2(speed, 0);
+            }
+            else
+            {
+                GameObject bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(0, 0, 180));
+                Rigidbody2D bi = bulletInstance.GetComponent<Rigidbody2D>();
+                bi.velocity = new Vector2(-speed, 0);
+                //Rigidbody2D bi = bulletInstance as Rigidbody2D;
+            }
+        }
+
+    }
+}
